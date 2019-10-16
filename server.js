f034d83fb77fe187241056021304e8099fdb8416
ryan-rozario <ryan.rozario1999@gmail.com>
2019-10-17 00:03:23 +0530

    added flag

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.get('/', (req, res) => {
 
 
 app.get('/success', (req, res) => {
-    res.send('You have completed the challenge');
+    res.send('Success. You have learnt how to modify cookies.\n\n flag: 3d1t_c0ok135  ');
 })
 
 
@@ -59,7 +59,7 @@ app.get('/success', (req, res) => {
 
 // route for handling 404 requests(unavailable routes)
 app.use(function (req, res, next) {
-    res.status(404).send("Sorry can't find that!")
+    res.status(404).send("HOW DID U GET HERE. THIS PAGE CANNOT BE FOUND.404 ERROR.A means of connecting a computer to any other computer anywhere in the world via dedicated routers and servers. When two computers are connected over the Internet, they can send and receive all kinds of information such as text, graphics, voice, video, and computer programs. No one owns Internet, although several organizations the world over collaborate in its functioning and development. The high-speed, fiber-optic cables (called backbones) through which the bulk of the Internet data travels are owned by telephone companies in their respective countries.    The Internet grew out of the Advanced Research Projects Agency's Wide Area Network (then called ARPANET) established by the US Department Of Defense in 1960s for collaboration in military research among business and government laboratories.Later universities and other US institutions connected to it. This resulted in ARPANET growing beyond everyone's expectations and acquiring the name Internet.The development of hypertext based technology (called World Wide web, WWW, or just the Web) provided means of displaying text, graphics, and animations, and easy search and navigation tools that triggered Internet's explosive worldwide growth.")
 });
 
 
