d677219eb290647f06d7214ad76a715f2652627f
ryan-rozario <ryan.rozario1999@gmail.com>
2019-10-16 23:39:09 +0530

    DYNAMIC PORT

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 
+const PORT = process.env.PORT || 3000;
+
 // invoke an instance of express application.
 var app = express();
 
 // set our application port
-app.set('port', 9000);
+app.set('port', PORT);
 
 // initialize body-parser to parse incoming parameters requests to req.body
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -53,64 +55,6 @@ app.get('/success', (req, res) => {
 })
 
 
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-// initialize express-session to allow us track the logged-in user across sessions.
-app.use(session({
-    key: 'user_sid',
-    secret: 'somerandonstuffs',
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-        expires: 600000
-    }
-}));
-
-
-// This middleware will check if user's cookie is still saved in browser and user is not set, then automatically log the user out.
-// This usually happens when you stop your express server after login, your cookie still remains saved in the browser.
-app.use((req, res, next) => {
-    if (req.cookies.user_sid && !req.session.user) {
-        res.clearCookie('user_sid');        
-    }
-    next();
-});
-
-
-// middleware function to check for logged-in users
-var sessionChecker = (req, res, next) => {
-    if (req.session.user && req.cookies.user_sid) {
-        res.redirect('/dashboard');
-    } else {
-        next();
-    }    
-};
-
-//JSON object to be added to cookie 
-let users = "admin" 
-
-// route for Home-Page
-app.get('/', sessionChecker, (req, res) => {
-    res.cookie("user_id", users);  
-    res.redirect('/cookie_factory');
-});
-
-
-
-
-
-*/
 
 
 // route for handling 404 requests(unavailable routes)
